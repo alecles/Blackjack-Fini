78d7fa6d8c56b28351f97577176378a6f2ff0e9d
lesal1831648 <lesal1831648@etu.cegepjonquiere.ca>
2018-10-12 23:26:14 -0400

    Ajoutez des fichiers projet.

---
+++ Blackjack Fini/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Blackjack_Fini
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
